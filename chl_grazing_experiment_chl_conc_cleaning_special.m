ad09add360b7ede712e8bf50b5cbce4514c88b12
pmarrec <67645223+pmarrec@users.noreply.github.com>
2023-01-25 20:10:43 -0500

    Add files via upload

--- chl_grazing_experiment_chl_conc_cleaning_special.m
+++ chl_grazing_experiment_chl_conc_cleaning_special.m
@@ -31,7 +31,7 @@
 clearvars, clc, close all
 
 %Set the directory where we work
-rep = 'C:\Users\pierr\My Drive\NES-LTER_Chla_Cleaning_Rates_Computation\';
+rep = 'C:\Users\pierr\Desktop\PostDoc_URI_Desktop\NES-LTER\NES-LTER_Chla_Cleaning_Rates_Computation\';
 %Set the directory where the input raw data are
 rep1 = strcat(rep,'chl-grazing-experiment-chl-conc-clean\');
 %Set the directory where the output clean data are
